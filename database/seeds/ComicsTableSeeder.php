d26258d69200bb47be955804dc5d6029c0ca7e7e
LucaSpennato <luca.spennato@gmail.com>
2022-09-13 20:52:04 +0200

    slug w/title

--- database/seeds/ComicsTableSeeder.php
+++ database/seeds/ComicsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Models\Comic;
+use Illuminate\Support\Str;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -15,13 +16,14 @@ class ComicsTableSeeder extends Seeder
     {
         for ($i=0; $i < 30; $i++) { 
             $newComic = new Comic();
-            $newComic->title = $faker->words(5, true);
+            $newComic->title = $faker->unique()->words(5, true);
             $newComic->description = $faker->paragraphs(2, true);
             $newComic->thumb = $faker->imageUrl(360, 360, 'comic');
             $newComic->price = $faker->randomFloat(2, 1, 99);
             $newComic->series = $faker->sentence(3);
             $newComic->sale_date = $faker->date('Y-m-d');
             $newComic->type = $faker->sentence(2);
+            $newComic->slug = Str::slug($newComic->title, '-') . "-" . $i;
             $newComic->save();
         }
     }
