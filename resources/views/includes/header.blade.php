5e39c410d138dcb3e7cb046b55b376c58b1bfc59
LucaSpennato <luca.spennato@gmail.com>
2022-09-14 15:41:15 +0200

    ternary request navbar

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -7,15 +7,13 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="{{ route('admin') }}">Home</a>
+            <a class="nav-link {{ request()->routeIs('admin') ? 'active text-primary' : '' }}" aria-current="page" href="{{ route('admin') }}">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{ route('comic.index') }}">Comics</a>
+            <a class="nav-link {{ request()->routeIs('comic.index') ? 'active text-primary' : '' }}" href="{{ route('comic.index') }}">Comics</a>
           </li>
           <li class="nav-item">
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
+            <a class="nav-link {{ request()->routeIs('') ? 'active text-warning' : '' }}">Disabled</a>
           </li>
         </ul>
       </div>
