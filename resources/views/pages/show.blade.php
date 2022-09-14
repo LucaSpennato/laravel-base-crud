dc8b79398dafe0bd805fcb361a1522f01cd0e10d
LucaSpennato <luca.spennato@gmail.com>
2022-09-14 17:59:52 +0200

    edit route in show comic href

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -13,7 +13,7 @@
                   <h6 class="card-subtitle mb-2 text-muted">{{ $comic->series }}</h6>
                   <p class="card-text">{{ $comic->description }}</p>
                   <p class="card-text">{{ $comic->price }}</p>
-                  <a href="#" class="card-link">Edit</a>
+                  <a href="{{ route('comic.edit', $comic->slug) }}" class="card-link">Edit</a>
                 </div>
               </div>
         </div>
